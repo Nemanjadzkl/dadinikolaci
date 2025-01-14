2d564d9f0562a89c1e5a775de601275b729a2313
nemanja.dzekulic <Nemanjadzkl@users.noreply.github.com>
2025-01-14 21:21:20 +0100

    Added email functionality and checkout flow- Testiranje

--- next.config.js
+++ next.config.js
@@ -1,10 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    reactStrictMode: true,
-    images: {
-      domains: ['res.cloudinary.com'],
-    },
-  }
-  
-  module.exports = nextConfig
-  
+  reactStrictMode: true,
+  images: {
+    domains: ['res.cloudinary.com'],
+  },
+  webpack: (config) => {
+    config.resolve.fallback = {
+      ...config.resolve.fallback,
+      fs: false,
+    };
+    return config;
+  },
+}
+
+module.exports = nextConfig
